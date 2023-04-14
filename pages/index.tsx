7af8ec70230ace7382acdab797695f24bfb7a77d
MJD-D <matthewdalby1995+techtest@gmail.com>
2023-04-14 17:19:14 +0100

    added a key skils section and some tailwind styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,28 +7,41 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main >
-      <div className='navbar'>
+    <main className='page-container'>
+      <div className='navbar bg-gray-500'>
         <li>
-            <Link href="/">Home</Link>
+            <Link className="hover:underline" href="/">Home</Link>
         </li>
         <li>
-            <Link href="/about" >About me</Link>
+            <Link className="hover:underline" href="/about" >About me</Link>
         </li>
         <li>
-            <Link href="/blog">Blog</Link>
+            <Link className="hover:underline" href="/blog">Blog</Link>
         </li>
       </div>
-      <div>
+      <div className='bg-gray-400'>
         <title>Matthew James Douglas-Dalby</title>
-        <h1> Matthew James Douglas-Dalby</h1>
-
+        <p className="text-2xl font-semibold hover:underline"> Matthew James Douglas-Dalby</p>
+      <div className='image-container bg-gray-400'>
       <Image src={profilePic}
       alt="picture of Matthew J Douglas-Dalby"
       />
+      <div className='skills-container'>
+        <h2> key skills</h2>
+        <li>
+        strong understanding of UX principles and UI design
+        </li>
+        <li>
+        Strong understanding of component based design models
+        </li>
+        <li>
+        Proficient with React, Vue, JS, java, C, Python
+        </li>
+      </div>
+      </div>
         
       </div>
-      <div className='contact-details'>
+      <div className='contact-details bg-gray-500 '>
         <div>
           email
         </div>
